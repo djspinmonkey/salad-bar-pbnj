555940dfa55fd8ad4afa067ea4b52458249aa758
John Hyland <john.hyland@lightstep.com>
2024-05-15 15:15:11 -0700

    Initial creation - it is still super busted!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider, useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
+// Set up Apollo client
+const client = new ApolloClient({
+    uri: 'https://2585-136-226-79-97.ngrok-free.app',
+});
+
+// Define your GraphQL query
+const MY_QUERY = gql`
+    {
+        organization(id:"LightStep") {
+            project(id:"dev-jhyland") {
+                alerts {
+                    name
+                    associatedCIs {
+                        ciIdentifier {
+                            sysId
+                            className
+                        }
+                        name
+                        assetTag
+                        assetLink
+                    }
+                }
+            }
+        }
+    }
+`;
+
+// Create a component that fetches and displays data
+function MyComponent() {
+    const { loading, error, data } = useQuery(MY_QUERY);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
+    // Replace 'data.items' with the actual path to the items in your query result
+    return (
+        <table>
+            {data.items.map(item => (
+                <tr>
+                    {/* Replace 'item.field' with the actual fields you want to display */}
+                    <td>{item.field1}</td>
+                    <td>{item.field2}</td>
+                    <td>{item.field3}</td>
+                </tr>
+            ))}
+        </table>
+    );
+}
+
+// Wrap your application in ApolloProvider
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ApolloProvider client={client}>
+            <MyComponent />
+        </ApolloProvider>
+    );
 }
 
 export default App;
